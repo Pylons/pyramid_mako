80d0e44e29e58d7212ee8878527bc613e1b97862
Bert JW Regeer <bertjw@regeer.org>
2014-04-15 14:34:00 -0600

    Prep for 1.0

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ requires = [
 ]
 
 setup(name='pyramid_mako',
-      version='1.0a3',
+      version='1.0',
       description='Mako template bindings for the Pyramid web framework',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
