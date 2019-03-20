419626c7014b6e3ae1d0fffb38c79e0695029e76
gebeto <slavik.nychkalo@gmail.com>
2019-03-20 18:17:16 +0200

    Add cache_args to config file

--- src/pyramid_mako/__init__.py
+++ src/pyramid_mako/__init__.py
@@ -191,6 +191,11 @@ def parse_options_from_settings(settings, settings_prefix, maybe_dotted):
     strict_undefined = asbool(sget("strict_undefined", False))
     preprocessor = sget("preprocessor", None)
     preprocessor_wants_settings = asbool(sget("preprocessor_wants_settings", None))
+    cache_type = sget('cache.type', None)
+    cache_dir = sget('cache.dir', module_directory)
+    cache_timeout = sget('cache.timeout', None)
+    cache_url = sget('cache.url', None)
+
     if not is_nonstr_iter(directories):
         # Since we parse a value that comes from an .ini config,
         # we treat whitespaces and newline characters equally as list item separators.
@@ -237,6 +242,12 @@ def parse_options_from_settings(settings, settings_prefix, maybe_dotted):
         filesystem_checks=reload_templates,
         strict_undefined=strict_undefined,
         preprocessor=preprocessor,
+        cache_args=dict(
+            timeout=cache_timeout,
+            url=cache_url,
+            type=cache_type,
+            dir=cache_dir,
+        )
     )
 
 
